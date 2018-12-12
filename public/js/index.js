313d78262c2cb3ee677a8f55ec54da9e8e7af0fe
Shubham Kabra <shubham.kabra@velotio.com>
2018-12-12 13:47:33 +0530

    Create message events

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket = io();
+socket.on('connect',function() {
+    console.log('connected to server')
+
+    // socket.emit('createEmail',{
+    //     to:'mike@ex.com',
+    //     text:'create email please'
+    // })
+    socket.emit('createMessage',{
+        from:'hey bro',
+        text:'create email please'
+    })
+});
+socket.on('disconnect',function(){
+    console.log('disconnected to server')
+});
+
+// socket.on('newEmail',function(email){
+
+//     console.log('New Email',email)
+// })
+socket.on('newMessage',function(mess){
+
+    console.log('New Message',mess)
+})
--- server/server.js
+++ server/server.js
@@ -4,28 +4,44 @@ const express = require('express');
 const socketio = require('socket.io');
 
 
-var app =express();
+var app = express();
 
 var server = http.createServer(app);
 var io = socketio(server);
 
 
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname,'../public');
+const publicPath = path.join(__dirname, '../public');
 // console.log(__dirname + '/../public')
 // console.log(publicPath)
 
 app.use(express.static(publicPath));
 
 
-io.on('connection',(socket)=>{
-console.log('New user connect');
+io.on('connection', (socket) => {
+    console.log('New user connect');
+    // socket.emit('newEmail', {
+    //     from: 'shubham@gmail.com',
+    //     text: 'whats up'
+    // });
+    socket.emit('newMessage', {
+        from: 'hey bro',
+        text: 'whats up bro'
+    });
 
-socket.on('disconnect',()=>{
-    console.log('user disconnected to server')
-});
+    // socket.on('createEmail',(newEmail)=>{
+    //     console.log('create email',newEmail);
+    // })
+    socket.on('createMessage',(newMessage)=>{
+        console.log('create message',newMessage);
+    })
+
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected to server')
+    });
 })
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log('Server is up')
 })
